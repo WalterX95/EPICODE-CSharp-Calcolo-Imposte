2582ed5d8e2ce6f26a96e7aa1294517c89166ad4
WalterX95 <walter_1995@hotmail.it>
2025-02-14 14:10:06 +0100

    Primo Commit
    Devo solo modificare Program.cs

---
+++ CalcoloImposte/Models/Contribuente.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcoloImposte.Models;
+using CodiceFiscaleUtility;
+
+namespace CalcoloImposte.Models
+{
+    internal class Contribuente
+    {
+        private string Nome {  get; set; } = String.Empty;
+        private string Cognome { get; set;} = String.Empty;
+        private DateTime DataNascita { get; set; }
+        private string Sesso {  get; set; } = String.Empty;
+        private string ComuneResidenza {  get; set; } = String.Empty;
+
+        private string Provincia {  get; set; } = String.Empty;
+        private decimal RedditoAnnuale { get; set; } 
+
+        private decimal RedditoImposte { get; set; }
+       
+        /*Costruzione di un Costruttore per caloclare un reddito */
+
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string sesso, string comuneResidenza, string provincia ,decimal redditoAnnuale )
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            Sesso = sesso;
+            Provincia = provincia;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+
+        }
+
+        public string ComponiCodiceFiscale()
+        {
+            int LivelloOmocodia = 1;
+            var cf = new CodiceFiscale(Cognome, Nome, Sesso, DataNascita, ComuneResidenza, Provincia, LivelloOmocodia);
+            return cf.CodiceNormalizzato;
+        }
+         public decimal CalcoloImposte() {
+            switch(RedditoAnnuale)
+            {
+                case <= 15.000m :
+                    RedditoImposte = RedditoAnnuale * 23 / 100;
+                    break;
+                case <= 28.000m :
+                    RedditoImposte = RedditoAnnuale * 27 / 100;
+                    break;
+                case <= 38.000m:
+                    RedditoImposte = RedditoAnnuale * 38 / 100;
+                    break;
+                case <= 55.000m:
+                    RedditoImposte = RedditoAnnuale * 41 / 100;
+                    break;
+                case >= 75.000m:
+                    RedditoImposte = RedditoAnnuale * 41 / 100;
+                    break;
+            }
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Contribuente :"+Nome);
+            Console.WriteLine("Nato il "+DataNascita.Date+" ("+Sesso+")");
+            Console.WriteLine("Codice Fiscale :"+ComponiCodiceFiscale());
+            Console.WriteLine("Reddito Dichiarato :" + RedditoAnnuale.ToString("C", new System.Globalization.CultureInfo("it-IT")));
+            Console.WriteLine("Imposta da Versare :" + RedditoImposte.ToString("C", new System.Globalization.CultureInfo("it-IT")));
+
+            return RedditoAnnuale;
+        }
+    }
+}
---
+++ CalcoloImposte/Program.cs
@@ -0,0 +1,33 @@
+﻿using CalcoloImposte.Models;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string? nomePersona;
+string? cognomePersona;
+string? sessoPersona;
+string? dateTime;
+string? comunePersona;
+string? provinciaPersona;
+string? reddito;
+
+Console.WriteLine("=============  CALCOLO REDDITO  =====================");
+Console.WriteLine("Inserire il Nome ");
+nomePersona = Console.ReadLine();
+Console.WriteLine("Inserire il Cognome ");
+cognomePersona = Console.ReadLine();
+Console.WriteLine("Inserire la Data Di Nascita ");
+dateTime = Console.ReadLine();
+Console.WriteLine("Inserire il Sesso ");
+sessoPersona = Console.ReadLine();
+Console.WriteLine("Inserisci Comune ");
+comunePersona = Console.ReadLine();
+Console.WriteLine("Inserisci Provincia ");
+provinciaPersona = Console.ReadLine();
+Console.WriteLine("Inserisci il Reddito ");
+reddito = Console.ReadLine();
+
+Contribuente RedditoCalcolato = new Contribuente(nomePersona, cognomePersona, Convert.ToDateTime(dateTime),sessoPersona,comunePersona,provinciaPersona,Convert.ToDecimal(reddito));
+
+RedditoCalcolato.CalcoloImposte();
+
+
+
